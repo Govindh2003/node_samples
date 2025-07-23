e784b6a8847bda8ead598450aa93cf47b6060c31
unknown <govindpoomukham@gmail.com>
2025-07-23 19:44:06 +0530

    adding

---
+++ natural.js
@@ -0,0 +1,17 @@
+function natural(n) {
+    let sum=0
+for (i = 1; i <= n; i++) {
+sum = sum+i
+ }
+console.log(sum)
+}
+
+natural(9)
+natural(5)
+natural(2)
+natural(4)
+natural(77)
+natural(6)
+natural(11)
+natural(32)
+
