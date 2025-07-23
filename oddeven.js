5dbd0acae08bf108cc6d4c87fd2a811fd7a2eaa3
unknown <govindpoomukham@gmail.com>
2025-07-23 19:46:06 +0530

    ODD EVEN

---
+++ oddeven.js
@@ -0,0 +1,15 @@
+function oddeven(num){
+if (num%2==0){
+    console.log(`${num}is even`)
+}
+else{
+    console.log(`${num}is odd`)
+}
+}
+
+oddeven(10)
+oddeven(2)
+oddeven(4)
+oddeven(6)
+oddeven(5)
+oddeven(9)
